a80e843a3003e174880501a53e6ccf83eea2bf15
Aleksandr Muravev <muravevkot@gmail.com>
2022-11-16 08:55:35 +0300

    fix Param Tests

--- Sprint_7/src/test/java/com/example/OrderTest.java
+++ Sprint_7/src/test/java/com/example/OrderTest.java
@@ -1,52 +1,23 @@
 package com.example;
 
 import io.restassured.response.ValidatableResponse;
-
 import org.example.client.OrderClient;
-
-import org.example.generators.OrderGenerator;
-import org.example.model.Order;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-import static org.apache.http.HttpStatus.SC_CREATED;
 import static org.apache.http.HttpStatus.SC_OK;
 
-@RunWith(Parameterized.class)
 public class OrderTest extends BaseTest {
 
-    private final Order order;
-    private final int statusCode;
+
     private OrderClient orderClient;
     private Object message;
-    private Order orderDefault;
-    private int track;
 
-
-    public OrderTest(Order order, int statusCode) {
-        this.order = order;
-        this.statusCode = statusCode;
-    }
-
-    // test data
-    @Parameterized.Parameters
-    public static Object[][] getTestData() {
-        return new Object[][]{
-                {OrderGenerator.getOrderBlack(), SC_CREATED},
-                {OrderGenerator.getDefault(), SC_CREATED},
-                {OrderGenerator.getOrderBlackGrey(), SC_CREATED},
-
-        };
-    }
 
     @Before
     public void setUp() {
         orderClient = new OrderClient();
-//        orderDefault = OrderGenerator.getDefault();
-
     }
 
     @Test
@@ -59,19 +30,6 @@ public class OrderTest extends BaseTest {
 
         Assert.assertNotNull("Список заказов пуст", message);
         Assert.assertEquals("Статус код не 200", SC_OK, statusCodeActual);
-
-    }
-
-    @Test
-    public void createOrder_Ok() {
-        ValidatableResponse responseCreate = orderClient.createOrder(order);
-
-        track = trackExtract(responseCreate);
-        int statusCodeActual = getStatusCodeActual(responseCreate);
-
-
-        Assert.assertTrue("Нет номера отслеживания", track > 0);
-        Assert.assertEquals("Статус код не 201", statusCode, statusCodeActual);
 
     }
 
---
+++ Sprint_7/src/test/java/com/example/OrderTestParam.java
@@ -0,0 +1,61 @@
+package com.example;
+
+import io.restassured.response.ValidatableResponse;
+import org.example.client.OrderClient;
+import org.example.generators.OrderGenerator;
+import org.example.model.Order;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.apache.http.HttpStatus.SC_CREATED;
+
+@RunWith(Parameterized.class)
+public class OrderTestParam extends BaseTest {
+
+    private final Order order;
+    private final int statusCode;
+    private OrderClient orderClient;
+    private Object message;
+    private int track;
+
+
+    public OrderTestParam(Order order, int statusCode) {
+        this.order = order;
+        this.statusCode = statusCode;
+    }
+
+    // test data
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {OrderGenerator.getOrderBlack(), SC_CREATED},
+                {OrderGenerator.getDefault(), SC_CREATED},
+                {OrderGenerator.getOrderBlackGrey(), SC_CREATED},
+
+        };
+    }
+
+    @Before
+    public void setUp() {
+        orderClient = new OrderClient();
+    }
+
+
+    @Test
+    public void createOrder_Ok() {
+        ValidatableResponse responseCreate = orderClient.createOrder(order);
+
+        track = trackExtract(responseCreate);
+        int statusCodeActual = getStatusCodeActual(responseCreate);
+
+
+        Assert.assertTrue("Нет номера отслеживания", track > 0);
+        Assert.assertEquals("Статус код не 201", statusCode, statusCodeActual);
+
+    }
+
+
+}
